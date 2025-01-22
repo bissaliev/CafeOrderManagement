5a9b21ad4e6e2d687de261def60434f3f72ccddb
bissaliev <bissaliev21@gmail.com>
2025-01-22 07:54:16 +0400

    Создал модель Dish

--- cafe/menu/models.py
+++ cafe/menu/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Dish(models.Model):
+    """Модель блюд"""
+
+    name = models.CharField("Название блюда", max_length=250, unique=True)
+    description = models.TextField("Описание блюда")
+    price = models.DecimalField(
+        "Стоимость блюда", max_digits=10, decimal_places=2
+    )
+    is_active = models.BooleanField("Активно", default=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Блюдо"
+        verbose_name_plural = "Блюда"
