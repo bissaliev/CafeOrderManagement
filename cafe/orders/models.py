5fe5beadd3fc359bf850777e7910d199d11722f5
bissaliev <bissaliev21@gmail.com>
2025-01-22 16:06:25 +0400

    Добавил методы в модели Order, OrderItem для получения общей стоимости

--- cafe/orders/models.py
+++ cafe/orders/models.py
@@ -14,12 +14,6 @@ class Order(models.Model):
         READY = "Готово"
         PAID = "Оплачено"
 
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="orders",
-        verbose_name="покупатель",
-    )
     table_number = models.CharField(
         "номер стола", max_length=10, db_index=True
     )
@@ -34,8 +28,16 @@ class Order(models.Model):
     updated = models.DateTimeField("время и дата обновления", auto_now=True)
 
     class Meta:
+        ordering = ["-created"]
         verbose_name = "Заказ"
         verbose_name_plural = "Заказы"
+
+    def __str__(self):
+        return f"Order({self.id}) - {self.status}"
+
+    def get_total_price(self):
+        """Получение общей суммы заказа"""
+        return sum(i.total_price for i in self.items.all())
 
 
 class OrderItem(models.Model):
@@ -47,9 +49,22 @@ class OrderItem(models.Model):
         related_name="items",
         verbose_name="заказ",
     )
+    dish = models.ForeignKey(
+        "menu.Dish", on_delete=models.SET_NULL, null=True, verbose_name="блюдо"
+    )
     quantity = models.PositiveSmallIntegerField("количество", default=1)
     price = models.DecimalField("стоимость", max_digits=10, decimal_places=2)
 
     class Meta:
         verbose_name = "Позиция заказа"
         verbose_name_plural = "Позиции заказа"
+
+    def save(self, *args, **kwargs):
+        if not self.price:
+            self.price = self.dish.price
+        return super().save(*args, **kwargs)
+
+    @property
+    def total_price(self):
+        """Общая стоимости позиции"""
+        return self.price * self.quantity
