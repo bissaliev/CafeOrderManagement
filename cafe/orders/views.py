cd8fbf30d8a535b463258a523ac36aab610196ea
bissaliev <bissaliev21@gmail.com>
2025-01-22 16:02:18 +0400

    Добавил в OrderCreateView обработку formset OrderItemFormSet

--- cafe/orders/views.py
+++ cafe/orders/views.py
@@ -1,26 +1,25 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-from orders.forms import OrderCreate
-from orders.models import Order, OrderItem
+from orders.forms import OrderCreate, OrderItemFormSet
+from orders.models import Order
 
 
-class OrderCreateView(LoginRequiredMixin, CreateView):
-    queryset = Order
+class OrderCreateView(CreateView):
+    queryset = Order.objects.all()
     form_class = OrderCreate
     template_name = "orders/create.html"
     success_url = reverse_lazy("menu:dish_list")
 
     def form_valid(self, form):
         form = self.get_form()
-        order = form.save(commit=False)
-        order.user = self.request.user
-        order.save()
-        dishes = form.cleaned_data.get("items")
-        OrderItem.objects.bulk_create(
-            [
-                OrderItem(order=order, dish=dish, price=dish.price)
-                for dish in dishes
-            ]
-        )
+        order = form.save()
+        formset = OrderItemFormSet(self.request.POST, instance=order)
+        if formset.is_valid():
+            formset.save()
         return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        formset = OrderItemFormSet()
+        context["formset"] = formset
+        return context
