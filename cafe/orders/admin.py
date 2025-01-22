ba6337fdf150752698e3c55636f8fbaee3a35e2d
bissaliev <bissaliev21@gmail.com>
2025-01-22 16:10:34 +0400

    Зарегистрировал модель заказов в админке

--- cafe/orders/admin.py
+++ cafe/orders/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from orders.models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "status",
+        "table_number",
+        "created",
+        "updated",
+        "count_items",
+    )
+    list_editable = ("status", "table_number")
+    list_filter = ("status", "table_number", "created")
+    inlines = (OrderItemInline,)
+
+    @admin.display(description="Количество блюд")
+    def count_items(self, obj):
+        return obj.items.count()
