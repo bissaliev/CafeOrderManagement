048b0d1d42f092e99178cd7b7d6c9bb1abdee838
bissaliev <bissaliev21@gmail.com>
2025-01-22 16:05:37 +0400

    Зарегистрировал классы представления для заказов в orders.urls

--- cafe/orders/urls.py
+++ cafe/orders/urls.py
@@ -5,5 +5,16 @@ app_name = "orders"
 
 
 urlpatterns = [
-    path("", views.OrderCreateView.as_view(), name="order_create"),
+    path("", views.OrderListView.as_view(), name="order_list"),
+    path("create/", views.OrderCreateView.as_view(), name="order_create"),
+    path(
+        "<int:id>/update_status/",
+        views.OrderChangeStatusView.as_view(),
+        name="order_update_status",
+    ),
+    path(
+        "<int:pk>/delete/",
+        views.OrderDeleteView.as_view(),
+        name="order_delete",
+    ),
 ]
