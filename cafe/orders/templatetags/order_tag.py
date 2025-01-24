14fbcbbae1f41e30fa2304d0b1855471cf714d8e
bissaliev <bissaliev21@gmail.com>
2025-01-24 15:37:14 +0400

    Добавил корректный переход по страницам при пагинации

--- cafe/orders/templatetags/order_tag.py
+++ cafe/orders/templatetags/order_tag.py
@@ -1,8 +1,21 @@
+from django.forms.boundfield import BoundField
+from django.http import QueryDict
+from django.template.context import RequestContext
 from django.template.library import Library
 
 register = Library()
 
 
 @register.filter
-def addclass(field, css):
+def addclass(field: BoundField, css: str):
+    """Добавление CSS-класса тегу"""
     return field.as_widget(attrs={"class": css})
+
+
+@register.simple_tag(takes_context=True)
+def get_query(context: RequestContext):
+    """Получение QueryString из запроса для корректной работы пагинации"""
+    query_string: QueryDict = context["request"].GET.copy()
+    if "page" in query_string:
+        query_string.pop("page")
+    return query_string.urlencode()
