9af40f8c9fe833b8c7002d94feb57ab4faf05bde
bissaliev <bissaliev21@gmail.com>
2025-01-27 17:06:49 +0400

    Рефакторинг приложения menu: Сортировка импортов и докстринги

--- cafe/menu/urls.py
+++ cafe/menu/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from menu import views
 
 app_name = "menu"
--- cafe/menu/views.py
+++ cafe/menu/views.py
@@ -1,14 +1,19 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+
 from menu.models import Dish
 
 
 class DishListView(ListView):
+    """Список блюд"""
+
     template_name = "menu/list.html"
     queryset = Dish.objects.all()
     paginate_by = 12
 
 
 class DishDetailView(DetailView):
+    """Получение конкретного блюда по id"""
+
     queryset = Dish.objects.all()
     template_name = "menu/detail.html"
